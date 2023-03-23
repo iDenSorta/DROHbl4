116b6a0f2a07d738370a81935e841f28573b1d1f
iDenSorta <denispanyukov@gmail.com>
2023-03-23 19:30:45 +0300

    add: mob menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,28 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Logo from '../img/header/logo.png'
+import MobNav from "./MobNav"
+
+
 
 function Header() {
     return (
       <header className="bg-pink-200 fixed w-full px-8 lg:px-16 z-30 h-[100px] lg:h-[130px] flex items-center">
         <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
-        <Link to={'/DROHbl4'}>
-          <img src={Logo} alt='' />
-        </Link>
-        <h1>Cфоткаю вашу хуйню</h1>
-        <nav className="hidden lg:flex gap-10">
-          <Link to={'/DROHbl4'} className='text-[#696c6d] hover:text-primary transition '>Домой</Link>
-          <Link to={'/People'} className='text-[#696c6d] hover:text-primary transition '>Чечики</Link>
-          <Link to={'/Aircraft'} className='text-[#696c6d] hover:text-primary transition '>Дронычы</Link>
-          <Link to={'/Services'} className='text-[#696c6d] hover:text-primary transition '>Услуги</Link>
-          <Link to={'/About'} className='text-[#696c6d] hover:text-primary transition '>Обо мне</Link>
+          <Link to={'/DROHbl4'}>
+            <img src={Logo} alt='' />
+          </Link>
+          <h1>Cфоткаю вашу хуйню</h1>
+          <nav className="hidden xl:flex gap-10">
+            <Link to={'/DROHbl4'} className='text-[#696c6d] hover:text-primary transition '>Домой</Link>
+            <Link to={'/People'} className='text-[#696c6d] hover:text-primary transition '>Чечики</Link>
+            <Link to={'/Aircraft'} className='text-[#696c6d] hover:text-primary transition '>Дронычы</Link>
+            <Link to={'/Services'} className='text-[#696c6d] hover:text-primary transition '>Услуги</Link>
+            <Link to={'/About'} className='text-[#696c6d] hover:text-primary transition '>Обо мне</Link>
 
-        </nav>
+          </nav>
         </div>
+        <MobNav />
       </header>
     )
   }
